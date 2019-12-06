66fa775850e2e85364ee953d6874cc8c730a45e8
vlad kuznetsov <vlad.y.kuznetsov@gmail.com>
2019-12-06 16:07:13 -0500

    Fetch request added to Yelp API

---
+++ src/util/yelp.js
@@ -0,0 +1,37 @@
+//Client ID
+//x90Mj02E5AK6v-2dda_9cA
+
+//API Key
+const apiKey = 'QLOauxeIxkI44RC8hVOemtmUQOKL6CAy0j-Xe68fGKuWdIVWFYjooimUUuvuTcJxpUwkdAO5jsMjKEsdTo8TEJHFRTkOYvFdvX4dpmIlEMDC4ywZZF8-kI5nNRTqXXYx';
+
+const Yelp = {
+
+  search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+    {headers: {Authorization: `Bearer ${apiKey}`}}).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => {
+          console.log(business);
+           return {
+             id: business.id,
+             imageSrc: business.image_url,
+             name: business.name,
+             address: business.location.address1,
+             city: business.location.city,
+             state: business.location.state,
+             zipCode: business.location.zip_code,
+             category: business.categories[0].title,
+             rating: business.rating,
+             reviewCount: business.review_count
+          }
+        });
+      }
+
+    })
+  }
+
+};
+
+export default Yelp;
